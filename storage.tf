e5faf667e797811b93a7d12c002caa2ed68a90e3
Volha Dziatko <volha10.dziatko@gmail.com>
2024-09-30 00:32:39 +0200

    add tfvars

--- storage.tf
+++ storage.tf
@@ -1,5 +1,5 @@
-resource "aws_s3_bucket" "task_1_bucket" {
-  bucket = "task-1-my-bucket"
+resource "aws_s3_bucket" "sample_bucket" {
+  bucket = var.aws_sample_bucket_name
 
   tags = {
     app = "rsschl"
