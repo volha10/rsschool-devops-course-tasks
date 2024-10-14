05179c5ff13c5141019ac768add2d1d94112ba15
Volha Dziatko <volha10.dziatko@gmail.com>
2024-10-14 03:03:44 +0200

    fix aws instance security group parameter

--- instance.tf
+++ instance.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "public_server_1" {
   ami             = "ami-0592c673f0b1e7665" # al2023-ami-2023.5.20241001.1-kernel-6.1-x86_64
   instance_type   = "t2.micro"
   subnet_id       = aws_subnet.public_subnet_1.id
-  security_groups = [aws_security_group.allow_ssh.name]
+  security_groups_ids = [aws_security_group.allow_ssh.id]
 
   tags = {
     Name = "public_server_1"
@@ -14,7 +14,7 @@ resource "aws_instance" "public_server_2" {
   ami             = "ami-0592c673f0b1e7665" # al2023-ami-2023.5.20241001.1-kernel-6.1-x86_64
   instance_type   = "t2.micro"
   subnet_id       = aws_subnet.public_subnet_2.id
-  security_groups = [aws_security_group.allow_ssh.name]
+  security_groups_ids = [aws_security_group.allow_ssh.id]
 
   tags = {
     Name = "public_server_2"
