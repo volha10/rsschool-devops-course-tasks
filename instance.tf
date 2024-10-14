97515cb6e7a168638e61abe01618e22d926b8aee
Volha Dziatko <volha10.dziatko@gmail.com>
2024-10-14 02:50:31 +0200

    fix aws ami id

--- instance.tf
+++ instance.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "public_server_1" {
-  ami             = "ami-0fff1b9a61dec8a5f" # al2023-ami-2023.5.20241001.1-kernel-6.1-x86_64
+  ami             = "ami-0592c673f0b1e7665" # al2023-ami-2023.5.20241001.1-kernel-6.1-x86_64
   instance_type   = "t2.micro"
   subnet_id       = aws_subnet.public_subnet_1.id
   security_groups = [aws_security_group.allow_ssh.name]
@@ -11,7 +11,7 @@ resource "aws_instance" "public_server_1" {
 }
 
 resource "aws_instance" "public_server_2" {
-  ami             = "ami-0fff1b9a61dec8a5f" # al2023-ami-2023.5.20241001.1-kernel-6.1-x86_64
+  ami             = "ami-0592c673f0b1e7665" # al2023-ami-2023.5.20241001.1-kernel-6.1-x86_64
   instance_type   = "t2.micro"
   subnet_id       = aws_subnet.public_subnet_2.id
   security_groups = [aws_security_group.allow_ssh.name]
